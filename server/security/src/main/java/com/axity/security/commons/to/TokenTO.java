1b54a91f8ad975c2dbba4ea3b1dd5d122541aa16
Ivan Equihua <Luis.HerreraE@axity.com>
2019-03-25 17:46:20 -0600

    Objeto de transporte de token.

---
+++ server/security/src/main/java/com/axity/security/commons/to/TokenTO.java
@@ -0,0 +1,13 @@
+package com.axity.security.commons.to;
+
+public class TokenTO {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
